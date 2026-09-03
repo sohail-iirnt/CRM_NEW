e2abc2fa0c64123e8c3c1ee904803ab0d92bdfad
Sohail Kachhi <43206109+sohail-iirnt@users.noreply.github.com>
2026-09-03 15:17:48 +0530

    Add forward order workflow status

--- src/config.js
+++ src/config.js
@@ -21,7 +21,7 @@ export const TICKET_TYPES = Object.freeze({
   OUTBOUND: 'outbound',
 })
 
-export const INBOUND_SOURCES = ['Haridwar', 'Shirwal', 'Khalapur', 'Supplier', 'Other']
+export const INBOUND_SOURCES = ['Haridwar', 'Shirwal', 'Shirval', 'Khalapur', 'Supplier', 'Other']
 export const DESTINATION_COMPANIES = ['5000', 'Other']
 export const PDI_RESULTS = ['PDI OK', 'ALL MATERIAL DAMAGE', 'NO STOCK', 'Other']
 export const DISPATCH_METHODS = ['Dedicated', 'Courier']
@@ -29,6 +29,7 @@ export const DISPATCH_METHODS = ['Dedicated', 'Courier']
 export const STATUSES = Object.freeze({
   PENDING_WAREHOUSE: 'pending_warehouse',
   READY_FOR_PDI: 'ready_for_pdi',
+  FORWARD_ORDER_PENDING: 'forward_order_pending',
   READY_FOR_DISPATCH: 'ready_for_dispatch',
   LOGISTICS_PENDING: 'logistics_pending',
   IN_TRANSIT: 'in_transit',
@@ -38,6 +39,7 @@ export const STATUSES = Object.freeze({
 export const STATUS_LABELS = Object.freeze({
   [STATUSES.PENDING_WAREHOUSE]: 'Pending Warehouse',
   [STATUSES.READY_FOR_PDI]: 'Ready for PDI',
+  [STATUSES.FORWARD_ORDER_PENDING]: 'Forward Order Pending',
   [STATUSES.READY_FOR_DISPATCH]: 'Ready for Dispatch',
   [STATUSES.LOGISTICS_PENDING]: 'Logistics Pending',
   [STATUSES.IN_TRANSIT]: 'In Transit',
